73c1c89e3bd007fa457a80363c47e7ba0c123f8f
Arjun Gautam <arjun.gautam.1694@gmail.com>
2022-06-01 08:29:17 +0545

    init

---
+++ src/App.js
@@ -0,0 +1,249 @@
+import {
+  Header, Main, NavBar, NavItem, Hero, Section, Title,
+  PageContent, ContentTitle, ContentDescription, Logos,
+  Subtitle, Description, Button, Footer, HeroDescription
+} from './components/all'
+import Logo from './assets/logo.png'
+import EventInfo from './assets/event_info.png'
+import EventTimeline from './assets/event_timeline.png'
+import EventActivities from './assets/event_activities.png'
+import RobSaboye from './assets/Rob_savoye.png'
+import Petya from './assets/petya.png'
+import Ramya from './assets/ramya.png'
+import './App.css';
+
+import ParticleBackground from 'react-particle-backgrounds'
+
+import college1 from './assets/colleges/academia_logo.png'
+import college2 from './assets/colleges/Amrit_sceince_logo.png'
+import college3 from './assets/colleges/Apex-College.png'
+import college4 from './assets/colleges/bhaktapur_multiple_campus.png'
+import college5 from './assets/colleges/birendra_multiple_campus.png'
+import college6 from './assets/colleges/butwal_multiple_campus.png'
+import college7 from './assets/colleges/ICMS_samarpanacademy.png'
+import college8 from './assets/colleges/Kathford-International-College-of-Engineering-and-Management.jpeg'
+import college9 from './assets/colleges/Kathmandu_University_Logo.png'
+import college10 from './assets/colleges/Kec_kalimati.jpg'
+import college11 from './assets/colleges/logo-wrc.png'
+import college12 from './assets/colleges/NCIT_LOGO.jpeg'
+import college13 from './assets/colleges/Padma_kanya_logo.jpeg'
+import college14 from './assets/colleges/pn_campus.png'
+import college15 from './assets/colleges/sagarmatha_logo.png'
+import college16 from './assets/colleges/sagarmatha_science_tech_logo.jpg'
+import college17 from './assets/colleges/samriddhi-college-logo-nepal.jpeg'
+
+import club1 from './assets/clubs/i-ces-logo.png'
+import club2 from './assets/clubs/icmsym.png'
+import club3 from './assets/clubs/kec it club.jpeg'
+import club4 from './assets/clubs/KU_computer_club_logo.png'
+import club5 from './assets/clubs/KU_open_sorce_circle_logo.png'
+import club6 from './assets/clubs/NOSK_logo.png'
+import club7 from './assets/clubs/osac_logo.jpeg'
+import club8 from './assets/clubs/PK_IT_club_logo.jpeg'
+
+import org1 from './assets/orgs/HOT_OMH_Asia_Pacific_Logo_COLOUR.png'
+import org2 from './assets/orgs/Humanitarian OpenStreetMap Team HOT Logo.png'
+import org3 from './assets/orgs/KLL Logo.png'
+import org4 from './assets/orgs/oknp.svg'
+import org5 from './assets/orgs/open_tech_community_logo_for_white_bg.png'
+
+
+const college_list = [college1,
+college2,
+college3,
+college4,
+college5,
+college6,
+college7,
+college8,
+college9,
+college10,
+college11,
+college12,
+college13,
+college14,
+college15,
+college16,
+college17
+]
+const club_list =[
+  club1,
+  club2,
+  club3,
+  club4,
+  club5,
+  club6,
+  club7,
+  club8
+]
+
+const org_list = [
+  org1,
+  org2,
+  org3,
+  org4,
+  org5
+]
+
+function App(props) {
+  const settings4 = {
+    particle: {
+      particleCount: 100,
+      color: "#d68c38",
+      minSize: 2,
+      maxSize: 4
+    },
+    velocity: {
+      directionAngle: 0,
+      directionAngleVariance: 30,
+      minSpeed: 0.2,
+      maxSpeed: 4
+    },
+    opacity: {
+      minOpacity: 0,
+      maxOpacity: 0.5,
+      opacityTransitionTime: 5000
+    }
+  }
+  const schedule_form_link = '/#schedule'
+  const workshop_form_link = 'https://docs.google.com/forms/d/1b4wnGVfSXz35qB76Q8anWIrQCtrHE67Jdy06ITbz3vE/edit'
+  const registration_form_link = 'https://docs.google.com/forms/d/e/1FAIpQLSfHaH6JDEEl6Bct1032mdWmErJDF41ZDbrwTP34gQmNdnWjvg/viewform'
+  return (
+    <Main>
+      <Header>
+      <div style={{position:'absolute', width: '100%', height: 800, top:0, zIndex:0}}><ParticleBackground settings={settings4}/></div>
+        <NavBar logo={Logo}>
+          <NavItem link={registration_form_link}>
+            Registration
+          </NavItem>
+          <NavItem link='#schedule'>
+            Schedule
+          </NavItem>
+        </NavBar>
+        <Hero>
+          <Section>
+            <Title>
+              OSM Hackfest 2022
+            </Title>
+            <Subtitle>
+              Open Technologies for Public Good
+            </Subtitle>
+            <small><small>OpenStreetMap | Free and Open Source Software | Open Data</small></small>
+            <Description>
+              Happening in Kathmandu, NP<br/>
+              From June 20 to June 24<br/>
+            </Description>
+          </Section>
+          <Section>
+            <Button onClick={()=>window.location.href=registration_form_link}>
+              Register
+            </Button>
+            <Button onClick={()=>window.location.href=schedule_form_link}>
+              Schedule
+            </Button>
+          </Section>
+        </Hero>
+        <HeroDescription>
+          <ContentDescription>
+            <div style={{margin:'auto', maxWidth:960}}>
+              <h3>
+                OSM hackfest 2022 is to building solutions using OpenStreetMap
+                and other open data and open technologies for public good.
+              </h3>
+              <p>
+                This is a national competition that aims students to:
+                <ul>
+                  <li>Utilize the free and open source geographical database of the world to build impactful civic tools and technologies.</li>
+                  <li>Gain a deeper understanding to OSM & Open Technologies.</li>
+                  <li>Showcase ideas and skillsets in regional scale.</li>
+                </ul>
+              </p>
+            </div>
+          </ContentDescription>
+        </HeroDescription>
+      </Header>
+      <PageContent>
+        <Section>
+          <ContentTitle>
+            <span id="schedule">Event Details</span>
+          </ContentTitle>
+          <ContentDescription>
+            <img src={EventTimeline} style={{maxHeight: 250, paddingRight: 10, marginRight: 10}}/>
+          </ContentDescription>
+          <br/>
+          <br/>
+          <h3>Main Event</h3>
+          <ContentDescription>
+            <img src={EventInfo} style={{maxHeight: 250, borderRight: '1px solid #353F54', paddingRight: 10, marginRight: 10}}/>
+            <img src={EventActivities} style={{maxHeight: 250}}/>
+          </ContentDescription>
+            <br/>
+          <Button onClick={()=>window.location.href=registration_form_link}>
+            Register for this event
+          </Button>
+        </Section>
+        <Section>
+          <ContentTitle>
+            OSM Workshop (Pre-Event)
+          </ContentTitle>
+          <h4>Pre-Event Details</h4>
+          <ContentDescription>
+            <b>Date and time:</b> June 7,  2022 - 10:00 AM to 4:00 PM<br/>
+            <b>Location:</b> Hotel Himalaya (‪183471604‬) | OpenStreetMap<br/><br/>
+            <b>Presenters:</b> {/*Rob Savoye, Petya Kangalova & Ramya Ragupathy<br/>*/}
+            <div>
+              <img src={RobSaboye} style={{maxHeight: 150, border: '5px solid #131F37', margin: 10}}/>
+              <img src={Petya} style={{maxHeight: 150, border: '5px solid #131F37', margin: 10}}/>
+              <img src={Ramya} style={{maxHeight: 150, border: '5px solid #131F37', margin: 10}}/>
+            </div>
+            <br/>
+            <b>Aim of the workshop:</b>
+            <ol style={{maxWidth: 720,margin:'auto', textAlign: 'left'}}>
+              <li>Workshop will cover ideas of Free and Open Source Software development, introduction to OSM projects, contribution, extraction, analysis and visualisation of geospatial data.</li>
+              <li>Workshop will be interactive and will include hands on sessions.</li>
+            </ol>
+            <br/>
+            <b>Participation:</b>
+            <ol style={{maxWidth: 720,margin:'auto', textAlign: 'left'}}>
+              <li>We have limited seats for up to 60 participants. After you submit this form, we will contact you through email before the event with other details of the event.</li>
+              <li>As the events will be hands on, participants are expected to bring their laptops for the event.</li>
+            </ol>
+            <Button onClick={()=>window.location.href=workshop_form_link}>
+              Apply for this workshop
+            </Button>
+          </ContentDescription>
+        </Section>
+        <Section>
+          <ContentTitle>
+            Participating Colleges
+          </ContentTitle>
+          <ContentDescription>
+            <Logos items={college_list}/>
+          </ContentDescription>
+        </Section>
+        <Section>
+          <ContentTitle>
+            Supporting Student Clubs
+          </ContentTitle>
+          <ContentDescription>
+            <Logos items={club_list}/>
+          </ContentDescription>
+        </Section>
+        <Section>
+          <ContentTitle>
+            Organizers
+          </ContentTitle>
+          <ContentDescription>
+            <Logos items={org_list}/>
+          </ContentDescription>
+        </Section>
+      </PageContent>
+      <Footer>
+        <b>Facebook Page: </b> <a style={{color:'white'}} href="https://fb.com/opentechcommunity">Open Tech Community</a><br/>
+        <b>Website: </b> <a style={{color:'white'}} href="https://osmhackfest.github.io">OSM Hackfest 2022</a><br/>
+      </Footer>
+    </Main>
+  );
+}
+
+export default App;
---
+++ src/components/all.js
@@ -0,0 +1,57 @@
+export const Header = ({children}) => {
+  return(<div style={{backgroundColor: '#202020'}}>{children}</div>)
+}
+export const Main = ({children}) => {
+  return(<div>{children}</div>)
+}
+export const NavBar = ({children, logo}) => {
+  return(
+    <div style={{position:'relative', zIndex:1}}>
+      <img src={logo} style={{paddingLeft: 30, maxHeight:150, maxWidth: 250, textAlign:'left'}}/>
+      <ul style={{listStyleType: 'none', margin:0, padding:0, float: 'right', marginTop:30, marginRight:30}}>
+        {children}
+      </ul>
+    </div>)
+}
+export const NavItem = ({children, link}) => {
+  return(<li style={{display: 'inline'}}><a href={link} style={{color: '#F56526', textDecoration: 'none', padding: 10}}><span style={{borderBottom: '2px solid #999'}}>{children}</span></a></li>)
+}
+export const Hero = ({children}) => {
+  return(<div style={{position:'relative', zIndex:1, color: '#eee', textAlign:'center', paddingBottom: 70}}>{children}</div>)
+}
+export const HeroDescription = ({children}) => {
+  return(
+    <div style={{position:'relative', zIndex: 1,color: '#eee', backgroundColor:'#111', padding: 20}}>
+      <div style={{margin:'auto'}}>{children}</div>
+    </div>)
+}
+export const Section = ({children}) => {
+  return(<div>{children}</div>)
+}
+export const PageContent = ({children}) => {
+  return(<div style={{padding: 20,textAlign: 'center'}}>{children}</div>)
+}
+export const Title = ({children}) => {
+  return(<h1 style={{fontSize: 72, padding:0, margin:0}}>{children}</h1>)
+}
+export const ContentTitle = ({children}) => {
+  return(<h2 style={{color: '#333', paddingTop: 50}}>{children}</h2>)
+}
+export const Subtitle = ({children}) => {
+  return(<h3 style={{padding:0, margin:0, backgroundColor: '#F34423', width:'fit-content', margin: 'auto', padding: 5, borderRadius: 5}}>{children}</h3>)
+}
+export const Description = ({children}) => {
+  return(<p>{children}</p>)
+}
+export const ContentDescription = ({children}) => {
+  return(<p>{children}</p>)
+}
+export const Button = ({children, onClick}) => {
+  return(<button onClick={onClick} style={{fontSize:16, border: 'none', backgroundColor: '#F56526', color: '#fff', cursor:'pointer', padding: 10, margin: 5, borderRadius: 30}}>{children}</button>)
+}
+export const Footer = ({children}) => {
+  return(<div style={{backgroundColor:'#202020', padding: 50, color:'#eee'}}>{children}</div>)
+}
+export const Logos = ({items}) => {
+  return(<div>{items.map(item=><img src={item} style={{maxHeight: 80, maxWidth: 230, margin: 20}}/>)}</div>)
+}
